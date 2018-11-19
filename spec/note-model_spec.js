b68db9a4212c131ac565af218ce7b4e7c801b241
Benjamin Kearns <benjaminkearns@Benjamins-MacBook-Pro.local>
2018-11-19 15:35:03 +0000

    added first test

--- spec/note-model_spec.js
+++ spec/note-model_spec.js
@@ -2,7 +2,7 @@
   function testCreateANote() {
     var note = new Note();
     note.newNote("this is a note");
-    if (note.getNote() !== "this is a note") {
+    if (note.getNote(1) !== "this is a note") {
       throw new Error("note not created")
     } else {
       console.log(".")

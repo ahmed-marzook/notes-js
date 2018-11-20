fb6852f956589acd96e1828d0caeaec5ad2a2bf0
Ahmed Marzook <m4marzook@gmail.com>
2018-11-20 16:20:41 +0000

    display view implementation

--- spec/note-list-view_spec.js
+++ spec/note-list-view_spec.js
@@ -6,6 +6,23 @@ function testListView() {
   note.newNote("random note 2")
   list.storeNote(note.getNote())
   var noteViewer = new NoteListView(list.listNotes());
-  console.log(noteViewer.noteList)
+  if (noteViewer.noteList[0] === "random note 1" && noteViewer.noteList[1] === "random note 2"){
+    console.log(".")
+  } else {
+    throw new Error("List not displaying")
+  };
 };
+function testDisplayList() {
+  var note = new Note
+  var list = new NoteList
+  note.newNote("random note 1")
+  list.storeNote(note.getNote())
+  note.newNote("random note 2")
+  list.storeNote(note.getNote())
+  if () {
+
+  } else {
+
+  }
+}
 testListView();

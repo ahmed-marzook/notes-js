646ca54301ad5da43e3bc6eff43594a9125ea249
Ahmed Marzook <m4marzook@gmail.com>
2018-11-20 15:53:10 +0000

    Added List view files

--- spec/note-list_spec.js
+++ spec/note-list_spec.js
@@ -1,15 +1,14 @@
-(function(exports) {
-  function testStoreANote() {
-    var note = new Note
-    var list = new NoteList
-    note.newNote("this is a note")
-    list.storeNote(note.getNote())
-    var testList = list.listNotes()
-    if (testList[0] !== "this is a note") {
-      throw new Error("note was not added")
-    } else {
-      console.log(".")
-    }
-  };
-  testStoreANote();
-})(this);
+
+function testStoreANote() {
+  var note = new Note
+  var list = new NoteList
+  note.newNote("this is a note")
+  list.storeNote(note.getNote())
+  var testList = list.listNotes()
+  if (testList[0] !== "this is a note") {
+    throw new Error("note was not added")
+  } else {
+    console.log(".")
+  }
+};
+testStoreANote();
--- spec/note_spec.js
+++ spec/note_spec.js
@@ -1,12 +1,10 @@
-(function(exports) {
-  function testCreateANote() {
-    var note = new Note();
-    note.newNote("this is a note");
-    if (note.getNote() !== "this is a note") {
-      throw new Error("note not created")
-    } else {
-      console.log(".")
-    }
-  };
-  testCreateANote();
-})(this);
+function testCreateANote() {
+  var note = new Note();
+  note.newNote("this is a note");
+  if (note.getNote() !== "this is a note") {
+    throw new Error("note not created")
+  } else {
+    console.log(".")
+  }
+};
+testCreateANote();

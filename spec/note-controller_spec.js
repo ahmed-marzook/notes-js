a0647f4215664ac62705774e2d1ec5ff60e153bf
Benjamin Kearns <rapiidz1@gmail.com>
2018-11-22 16:15:54 +0000

    working on user input

--- spec/note-controller_spec.js
+++ spec/note-controller_spec.js
@@ -1,13 +1,13 @@
-function noteController() {
-  var note = new Note;
-  var noteList = new NoteList;
-  note.newNote("Favourite drink: seltzer");
-  noteList.storeNote(note.getNote());
-  var noteListView = new NoteListView(noteList);
-  document.getElementById('app').innerHTML = noteListView.displayListViewer();
-  if (document.getElementById('app').innerHTML === "<ul><li>Favourite drink: seltzer</li></ul>") {
-    console.log(".")
-  } else {
-    throw new Error("list not displayed on page")
-  }
-};
+// function noteController() {
+//   var note = new Note;
+//   var noteList = new NoteList;
+//   note.newNote("Favourite drink: seltzer");
+//   noteList.storeNote(note.getNote());
+//   var noteListView = new NoteListView(noteList);
+//   document.getElementById('app').innerHTML = noteListView.displayListViewer();
+//   if (document.getElementById('app').innerHTML === "<ul><li>Favourite drink: seltzer</li></ul>") {
+//     console.log(".")
+//   } else {
+//     throw new Error("list not displayed on page")
+//   }
+// };
